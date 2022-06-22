0a0e4c12f7af8674a91c0c71a12384e488d4d262
Juansepo13 <juancho_s1314@hotmail.com>
2022-06-22 01:42:58 -0500

    edit file

--- premiosplatzi/premiosplatziapp/premiosplatziapp/urls.py
+++ premiosplatzi/premiosplatziapp/premiosplatziapp/urls.py
@@ -1,9 +1,5 @@
-from textwrap import indent
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
-
-from premiosplatzi.premiosplatziapp.polls.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
