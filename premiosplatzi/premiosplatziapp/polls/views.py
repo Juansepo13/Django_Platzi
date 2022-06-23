10030bc9bd4b7669dbf78b612930717b983ce7ab
Juansepo13 <juancho_s1314@hotmail.com>
2022-06-23 10:29:41 -0500

    asd

--- premiosplatzi/premiosplatziapp/polls/views.py
+++
@@ -1,8 +0,0 @@
-from this import d
-from turtle import ht
-from zlib import decompressobj
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello World")
