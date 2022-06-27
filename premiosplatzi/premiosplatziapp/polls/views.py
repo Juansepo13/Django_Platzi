52fc55c29d9525d4c59cd0e02eddc27dc7b009ee
Juansepo13 <juancho_s1314@hotmail.com>
2022-06-27 18:10:26 -0500

    add views.py, and index.html

--- premiosplatzi/premiosplatziapp/polls/views.py
+++ premiosplatzi/premiosplatziapp/polls/views.py
@@ -1,8 +1,14 @@
+from unittest import load_tests
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Estas en la pagina principal de Premios Platzi App")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 
 def detail(request, question_id):
     return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
