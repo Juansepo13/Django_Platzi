914b51ab6ab911aa0fafd1b0041a6c048162282a
Juansepo13 <juancho_s1314@hotmail.com>
2022-06-28 18:24:47 -0500

    add changue in urls premios platzi app

--- premiosplatzi/premiosplatziapp/premiosplatziapp/urls.py
+++ premiosplatzi/premiosplatziapp/premiosplatziapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("premiosplatzi/", include("polls.urls"))
+    path("polls/", include("polls.urls"))
 ]
