5a9017a815ce1c041035d30bf812999eef6b3c5b
Juansepo13 <juancho_s1314@hotmail.com>
2022-06-28 12:45:42 -0500

    add file views

--- premiosplatzi/premiosplatziapp/polls/views.py
+++ premiosplatzi/premiosplatziapp/polls/views.py
@@ -1,8 +1,8 @@
 from unittest import load_tests
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -20,4 +20,15 @@ def result(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", { 
+        "question": question.id
+        "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
