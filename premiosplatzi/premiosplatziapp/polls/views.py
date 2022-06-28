92ed869629358b567a0a59c81de7079d9f62917f
Juansepo13 <juancho_s1314@hotmail.com>
2022-06-28 18:18:58 -0500

    add changues

--- premiosplatzi/premiosplatziapp/polls/views.py
+++ premiosplatzi/premiosplatziapp/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .models import Question, Choice
 
+from .models import Question, Choice
 
 
 def index(request):
@@ -11,18 +11,19 @@ def index(request):
         "latest_question_list": latest_question_list
     })
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {
         "question": question
     })
 
+
 def results(request, question_id):
     question = get_object_or_404(question, pk=question_id)
     return render(request, "polls/results.html", {
         "question": question
-    }
-    )
+    })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
