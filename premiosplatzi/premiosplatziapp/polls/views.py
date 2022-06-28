b4a3fb24fde31ec3fc5cd6fea6771f6ae76912ca
Juansepo13 <juancho_s1314@hotmail.com>
2022-06-28 13:04:51 -0500

    correct identation

--- premiosplatzi/premiosplatziapp/polls/views.py
+++ premiosplatzi/premiosplatziapp/polls/views.py
@@ -31,7 +31,7 @@ def vote(request, question_id):
         return render(request, "polls/detail.html", { 
             "question": question,
             "error_message": "No elegiste una respuesta"
-            })
+        })
     else:
         selected_choice.votes += 1
         selected_choice.save()
